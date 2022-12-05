a4e36a2930b23adc6b65d3c308cb7ad044145080
hpvlong <hophuocvietlong280999@gmail.com>
2022-12-05 16:59:58 +0700

    main | add gradle

---
+++ src/main/java/com/example/pipelineazure/PipelineAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipelineazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pipelineazure/PipelineAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipelineazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
