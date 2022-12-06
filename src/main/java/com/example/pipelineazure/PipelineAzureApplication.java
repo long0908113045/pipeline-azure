1f6cdc735d8cbaa85ba9d4314c6a7d7bf4368896
hpvlong <hophuocvietlong280999@gmail.com>
2022-12-06 08:57:47 +0700

    checkstyle | test check style

--- src/main/java/com/example/pipelineazure/PipelineAzureApplication.java
+++ src/main/java/com/example/pipelineazure/PipelineAzureApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PipelineAzureApplication {
 
 	public static void main(String[] args) {
+		String a = "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss";
 		SpringApplication.run(PipelineAzureApplication.class, args);
 	}
 
