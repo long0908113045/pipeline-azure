4a58aa5053ac93ece024b1038476801607614d4d
hpvlong <hophuocvietlong280999@gmail.com>
2022-12-06 14:38:13 +0700

    check-style-test | update code

--- src/test/java/com/example/pipelineazure/PipelineAzureApplicationTests.java
+++ src/test/java/com/example/pipelineazure/PipelineAzureApplicationTests.java
@@ -3,12 +3,15 @@ package com.example.pipelineazure;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class PipelineAzureApplicationTests {
 
 	@Test
 	void contextLoads() {
-    String a = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    String name = "Hello word";
+    assertEquals(name, "Product is created successfully");
 	}
 
 }
